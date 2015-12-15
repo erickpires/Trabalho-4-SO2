cc5b45884155cc8d51a9cfcb3b28eb05fcd9f37d
Erick Pires <pires@erickpires.com>
2015-12-14 23:21:50 -0200

    Ignoring all signals except for SIGTERM and SIGKILL. Also, exiting gracefully upon a SIG_TERM

--- przombies.c
+++ przombies.c
@@ -7,6 +7,15 @@
 
 #define FALSE 0
 #define TRUE 1
+#define N_SIG 31
+
+static int is_running = TRUE;
+static int terminated = FALSE;
+
+void stop_running(int signal) {
+	is_running = FALSE;
+	terminated = TRUE;
+}
 
 FILE* execute_command(const char* command) {
 	int file_descriptors[2];
@@ -27,6 +36,7 @@ FILE* execute_command(const char* command) {
 		close(file_descriptors[0]);
 		dup2(file_descriptors[1], 1);
 
+		// TODO: This is hardcoded
 		execlp("ps", "ps", "-el", NULL);
 
 		return NULL; // This line is never reached
@@ -40,7 +50,17 @@ FILE* execute_command(const char* command) {
 int main(int argc, char** argv){
 
 	// Let the child processes die and not become a zombie.
-	signal(SIGCHLD, SIG_IGN);
+	//signal(SIGCHLD, SIG_IGN);
+
+
+	// Ignoring all signals except SIG_TERM and SIG_KILL
+	// SIGCHLD is also ignore
+	for(int signal_num = 0; signal_num < N_SIG; signal_num++) {
+		if(signal_num != SIGTERM && signal_num != SIGKILL)
+			signal(signal_num, SIG_IGN);
+	}
+
+	signal(SIGTERM, stop_running);
 
 	int time_to_sleep;
 	if (argc != 2 ||  (time_to_sleep = atoi (argv[1])) <= 0) {
@@ -63,20 +83,20 @@ int main(int argc, char** argv){
 
 	fprintf(logfile, "Log:\n\n");
 	fprintf(logfile, "\tPID\t\tPPID\t\tNome do Programa\n");
-	fprintf(logfile, "===========================================================\n");
+	fprintf(logfile, "===========================================================");
 
 	//Main loop
-	while(TRUE) {
+	while(is_running) {
 		FILE* process_input_stream = execute_command("ps -el");
 
 		int first = TRUE;
 		while(!feof(process_input_stream)) {
 			if (first == TRUE) {
-				fgets(buffer, 100, process_input_stream);
+				fgets(buffer, sizeof(buffer), process_input_stream);
 				first = FALSE;
 			}
 
-			fgets(buffer, 100, process_input_stream);
+			fgets(buffer, sizeof(buffer), process_input_stream);
 
 			token = strtok(buffer, separator);
 		    int i = 0;
@@ -106,10 +126,17 @@ int main(int argc, char** argv){
 		    }
 		}
 
-		fprintf(logfile, "===========================================================\n");
+		fprintf(logfile, "\n===========================================================\n");
+		fclose(process_input_stream);
 
 		sleep(time_to_sleep);
 	}
+
+	if(terminated) {
+		fprintf(logfile, "The deamon has been closed by a SIG_TERM\n");
+	}
+
+	fclose(logfile);
 
     return 0;
 }
